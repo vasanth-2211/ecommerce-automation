b2985a75a4f2e5c9042a11cd9d02d2c6fd823b9a
vasanthramesh <vasanth9442tvr@gmail.com>
2026-03-13 19:45:46 +0530

    rolled back to fix some unneccessary tomato

---
+++ src/test/java/tests/CheckoutFlowTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.CartPage;
+import pages.CheckOutPage;
+import pages.InventoryPage;
+import pages.LoginPage;
+
+public class CheckoutFlowTest extends BaseTest {
+    @Test(groups = {"checkout","regression"})
+    public  void checkoutFlowTest(){
+        LoginPage loginPage = new LoginPage();
+        InventoryPage inventoryPage = loginPage.login(
+                "standard_user",
+                "secret_sauce"
+        );
+        inventoryPage.addProductToCart("Sauce Labs Bolt T-Shirt");
+        CartPage cartPage = inventoryPage.goToCart();
+        CheckOutPage checkOutPage = cartPage.clickCheckout();
+
+        Assert.assertEquals(checkOutPage.getCheckOutTitle(),
+                "Checkout: Your Information");
+        checkOutPage.fillCheckoutInformation(
+                "John",
+                "Doe",
+                "12345"
+        );
+    }
+
+}
