14e1b9ff9e87de66329548f4c980807a98b95dba
vasanthramesh <vasanth9442tvr@gmail.com>
2026-03-13 18:14:16 +0530

    Add AddMultipleProductsTest to verify multiple cart items

---
+++ src/test/java/tests/AddMultipleProductsTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.InventoryPage;
+import pages.LoginPage;
+
+public class AddMultipleProductsTest extends BaseTest {
+   @Test
+    public void addMultipleProducts() {
+       LoginPage loginPage = new LoginPage();
+       InventoryPage inventoryPage = loginPage.login(
+               "standard_user",
+               "secret_sauce"
+       );
+       inventoryPage.addProductToCart("Sauce Labs Backpack");
+       inventoryPage.addProductToCart("Sauce Labs Bike Light");
+       inventoryPage.addProductToCart("Sauce Labs Bolt T-Shirt");
+
+       String itemsCount = inventoryPage.getCartCount();
+       Assert.assertEquals(itemsCount,"3");
+
+   }
+}
