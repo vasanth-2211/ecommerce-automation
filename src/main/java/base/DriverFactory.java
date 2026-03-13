268ebaa1f45f2f8eee36a92d5a76c125e3837f9a
vasanthramesh <vasanth9442tvr@gmail.com>
2026-03-13 14:32:31 +0530

    Improve DriverFactory to support multiple browsers and clean driver lifecycle

--- src/main/java/base/DriverFactory.java
+++ src/main/java/base/DriverFactory.java
@@ -3,12 +3,24 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class DriverFactory {
     private static WebDriver driver;
-    public static WebDriver initializeDriver() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    public static WebDriver initializeDriver(String browser) {
+       if (browser.equalsIgnoreCase("chrome")) {
+           WebDriverManager.chromedriver().setup();
+           driver = new ChromeDriver();
+       }
+       else if (browser.equalsIgnoreCase("firefox")) {
+           WebDriverManager.firefoxdriver().setup();
+           driver = new FirefoxDriver();
+       }
+       else if (browser.equalsIgnoreCase("safari")) {
+           WebDriverManager.safaridriver().setup();
+           driver = new SafariDriver();
+       }
         driver.manage().window().maximize();
         return driver;
     }
@@ -19,6 +31,7 @@ public class DriverFactory {
     public static void quitDriver() {
         if (driver != null) {
             driver.quit();
+            driver = null;
         }
     }
 }
