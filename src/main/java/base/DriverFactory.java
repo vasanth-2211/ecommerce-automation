b10e57bd62c7d5d6cd675ecae51c3fd46ea94e92
vasanthramesh <vasanth9442tvr@gmail.com>
2026-03-13 13:55:01 +0530

    Implement Driver Factory for drivermanagement

---
+++ src/main/java/base/DriverFactory.java
@@ -0,0 +1,24 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+    private static WebDriver driver;
+    public static WebDriver initializeDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        return driver;
+    }
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
