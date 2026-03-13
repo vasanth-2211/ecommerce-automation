f9f9b75fc305ec6542c9ffd4239797c087e6fa5f
vasanthramesh <vasanth9442tvr@gmail.com>
2026-03-13 14:08:27 +0530

    Created Login page using POM

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,34 @@
+package pages;
+
+import base.DriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class LoginPage {
+    WebDriver driver;
+    public LoginPage(WebDriver driver) {
+        this.driver = DriverFactory.getDriver();
+        PageFactory.initElements(driver, this);
+    }
+    @FindBy(id="user-name")
+    WebElement username;
+    @FindBy(id="password")
+    WebElement password;
+    @FindBy(id="login-button")
+    WebElement loginButton;
+
+    public void enterUsername(String user) {
+        username.sendKeys(user);
+    }
+    public void enterPassword(String pass) {
+        password.sendKeys(pass);
+    }
+    public void clickLoginButton() {
+        loginButton.click();
+    }
+
+
+}
