32c091294fd9f44c8b99557facddf7481737899f
vasanthramesh <vasanth9442tvr@gmail.com>
2026-03-14 18:06:57 +0530

    Add ExtentReports reporting with TestNG listener and screenshot capture on failure

---
+++ src/main/java/utils/ExtentManager.java
@@ -0,0 +1,19 @@
+package utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentManager {
+    private static ExtentReports extent;
+    public static ExtentReports getExtentReport() {
+        if (extent == null) {
+            ExtentSparkReporter reporter = new ExtentSparkReporter("test-output/extent-report.html");
+            reporter.config().setReportName("Ecommerce Automation Report");
+            reporter.config().setDocumentTitle("Automation Test Results");
+
+            extent = new ExtentReports();
+            extent.attachReporter(reporter);
+        }
+    return extent;
+    }
+}
--- src/test/java/listeners/TestListener.java
+++ src/test/java/listeners/TestListener.java
@@ -1,13 +1,39 @@
 package listeners;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import utils.ExtentManager;
 import utils.ScreenshotUtils;
 
 public class TestListener implements  ITestListener {
+    private static ExtentReports extent = ExtentManager.getExtentReport();
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest extentTest = extent.createTest(result.getName());
+        test.set(extentTest);
+    }
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.get().pass("Test passed");
+    }
     @Override
     public void onTestFailure(ITestResult result) {
-        String testName = result.getName();
-        ScreenshotUtils.captureScreenshot(testName);
+        String screenshotPath = ScreenshotUtils.captureScreenshot(result.getName());
+        test.get().fail(result.getThrowable());
+
+        try{
+            test.get().addScreenCaptureFromPath(screenshotPath);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
+    @Override
+    public void onFinish(org.testng.ITestContext context) {
+        extent.flush();
+    }
+
 }
